82d090981031d1fd77d592d10e22a03379c542d3
Chia-Wei Cheng <z5390087@ad.unsw.edu.au>
2022-10-27 13:26:09 +1100

    add .wav converter

--- src/prog/src/main.c
+++ src/prog/src/main.c
@@ -29,11 +29,11 @@
 // #include "wav.h"
 
 
-#define TRANSFER_RUNS 10
+#define TRANSFER_RUNS 3750
 
 #define NUM_CHANNELS 2
 #define BPS 24
-#define SAMPLE_RATE 44100
+#define SAMPLE_RATE 48000
 #define RECORD_DURATION 10
 
 void bin(uint8_t n) {
@@ -64,10 +64,48 @@ void parsemem(void* virtual_address, int word_count) {
 
 }
 
+void create_wav(char *filename, uint32_t num_samples, uint16_t num_channels, uint32_t *data, uint32_t fs, uint16_t bit_depth) {
+    FILE *wav_file;
+    unsigned int bytes_per_sample = bit_depth / 8;
+    uint32_t byte_rate = fs * num_channels * bytes_per_sample;
+    uint32_t sub_chunk_1_size = 16;
+    uint16_t pcm_format = 1;
+    uint16_t block_align = num_channels * bytes_per_sample;
+    uint32_t sub_chunk_2_size = num_samples * num_channels * bytes_per_sample;
+    uint32_t chunk_size = 36 + sub_chunk_2_size;
+
+    wav_file = fopen(filename, "w+");
+    assert(wav_file);
+
+    fwrite("RIFF", 1, 4, wav_file);
+    fwrite(&chunk_size, sizeof(uint32_t), 1, wav_file);
+    fwrite("WAVE", 1, 4, wav_file);
+
+    fwrite("fmt ", 1, 4, wav_file);
+    fwrite(&sub_chunk_1_size, sizeof(uint32_t), 1, wav_file);
+    fwrite(&pcm_format, sizeof(uint16_t), 1, wav_file);
+    fwrite(&num_channels, sizeof(uint16_t), 1, wav_file);
+    fwrite(&fs, sizeof(uint32_t), 1, wav_file);
+    fwrite(&byte_rate, sizeof(uint32_t), 1, wav_file);
+    fwrite(&block_align, sizeof(uint16_t), 1, wav_file);
+    fwrite(&bit_depth, sizeof(uint16_t), 1, wav_file);
+
+    fwrite("data", sizeof(char), 4, wav_file);
+    fwrite(&sub_chunk_2_size, sizeof(uint32_t), 1, wav_file);
+
+    for (int i = 0; i < num_samples; i++) {
+        fwrite(&data[i], bytes_per_sample, 1, wav_file);
+    }
+
+    fclose(wav_file);
+}
+
 int main() {
     printf("Entered main\n");
 
     uint32_t frames[TRANSFER_RUNS][TRANSFER_LEN] = {0};
+    uint32_t *buffer = malloc(NUM_CHANNELS * SAMPLE_RATE * RECORD_DURATION * sizeof(uint32_t));
+    uint32_t count = 0;
 
     audio_i2s_t my_config;
     if (audio_i2s_init(&my_config) < 0) {
@@ -99,7 +137,15 @@ int main() {
     for (int i = 0; i < TRANSFER_RUNS; i++) {
         int32_t *samples = audio_i2s_recv(&my_config);
         memcpy(frames[i], samples, TRANSFER_LEN*sizeof(uint32_t));
+
+        for (int j = 0; j < TRANSFER_LEN; j++) {
+            buffer[count] = frames[i][j];
+            count++;
+        }
+
     }
+
+    create_wav("wav_test.wav", NUM_CHANNELS*SAMPLE_RATE*RECORD_DURATION, NUM_CHANNELS, buffer, SAMPLE_RATE, BPS);
 
     for (int i = 0; i < TRANSFER_RUNS; i++) {
         printf("Frame %d:\n", i);
