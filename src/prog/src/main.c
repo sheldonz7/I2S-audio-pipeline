657b5194601e888a7d147ffee518850ec2c132e6
Yuval Kansal <54937899+yuvalkansal@users.noreply.github.com>
2022-11-03 10:48:48 +1100

    Added code to capture microphone bitstream in a text file

--- src/prog/src/main.c
+++ src/prog/src/main.c
@@ -24,17 +24,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #include "audio_i2s.h"
 // #include "wav.h"
 
 
-#define TRANSFER_RUNS 10
+#define TRANSFER_RUNS 3750
 
 #define NUM_CHANNELS 2
 #define BPS 24
-#define SAMPLE_RATE 44100
+#define SAMPLE_RATE 48000
 #define RECORD_DURATION 10
+FILE *fptr;
 
 void bin(uint8_t n) {
     uint8_t i;
@@ -45,6 +48,7 @@ void bin(uint8_t n) {
 }
 
 void parsemem(void* virtual_address, int word_count) {
+    
     uint32_t *p = (uint32_t *)virtual_address;
     char *b = (char*)virtual_address;
     int offset;
@@ -56,18 +60,60 @@ void parsemem(void* virtual_address, int word_count) {
         sample_count = p[offset] >> 18;
 
         for (int i = 0; i < 4; i++) {
-            bin(b[offset*4+i]);
-            printf(" ");
+            // bin(b[offset*4+i]);
+            // printf(" ");
         }
-        printf(" -> [%d]: %02x (%dp)\n", sample_count, sample_value, sample_value*100/((1<<18)-1));
+        // printf(" -> [%d]: %02x (%dp)\n", sample_count, sample_value, sample_value*100/((1<<18)-1));
+        fprintf(fptr, "%d\n", (int) sample_value);
+
     }
 
 }
 
+void create_wav(char *filename, uint32_t num_samples, uint16_t num_channels, uint32_t *data, uint32_t fs, uint16_t bit_depth) {
+    FILE *wav_file;
+    unsigned int bytes_per_sample = bit_depth / 8;
+    uint32_t byte_rate = fs * num_channels * bytes_per_sample;
+    uint32_t sub_chunk_1_size = 16;
+    uint16_t pcm_format = 1;
+    uint16_t block_align = num_channels * bytes_per_sample;
+    uint32_t sub_chunk_2_size = num_samples * num_channels * bytes_per_sample;
+    uint32_t chunk_size = 36 + sub_chunk_2_size;
+
+    wav_file = fopen(filename, "w+");
+    assert(wav_file);
+
+    fwrite("RIFF", 1, 4, wav_file);
+    fwrite(&chunk_size, sizeof(uint32_t), 1, wav_file);
+    fwrite("WAVE", 1, 4, wav_file);
+
+    fwrite("fmt ", 1, 4, wav_file);
+    fwrite(&sub_chunk_1_size, sizeof(uint32_t), 1, wav_file);
+    fwrite(&pcm_format, sizeof(uint16_t), 1, wav_file);
+    fwrite(&num_channels, sizeof(uint16_t), 1, wav_file);
+    fwrite(&fs, sizeof(uint32_t), 1, wav_file);
+    fwrite(&byte_rate, sizeof(uint32_t), 1, wav_file);
+    fwrite(&block_align, sizeof(uint16_t), 1, wav_file);
+    fwrite(&bit_depth, sizeof(uint16_t), 1, wav_file);
+
+    fwrite("data", sizeof(char), 4, wav_file);
+    fwrite(&sub_chunk_2_size, sizeof(uint32_t), 1, wav_file);
+
+    for (uint32_t i = 0; i < num_samples; i++) {
+        fwrite(&data[i], bytes_per_sample, 1, wav_file);
+    }
+
+
+    fclose(wav_file);
+}
+
 int main() {
     printf("Entered main\n");
+    fptr = fopen("sample.txt", "w");
 
     uint32_t frames[TRANSFER_RUNS][TRANSFER_LEN] = {0};
+    uint32_t *buffer = (uint32_t*) malloc(NUM_CHANNELS * SAMPLE_RATE * RECORD_DURATION * sizeof(uint32_t));
+    uint32_t count = 0;
 
     audio_i2s_t my_config;
     if (audio_i2s_init(&my_config) < 0) {
@@ -87,7 +133,6 @@ int main() {
     
 
 
-    
     // audio_i2s_release(&my_config);
 
     // return 0;
@@ -99,15 +144,25 @@ int main() {
     for (int i = 0; i < TRANSFER_RUNS; i++) {
         int32_t *samples = audio_i2s_recv(&my_config);
         memcpy(frames[i], samples, TRANSFER_LEN*sizeof(uint32_t));
+
+        for (int j = 0; j < TRANSFER_LEN; j++) {
+            buffer[count] = frames[i][j];
+            count++;
+        }
+
+        parsemem(frames[i], TRANSFER_LEN);
+
     }
 
-    for (int i = 0; i < TRANSFER_RUNS; i++) {
-        printf("Frame %d:\n", i);
-        parsemem(frames[i], TRANSFER_LEN);
-        printf("==============================\n");
-    }
+    create_wav("wav_test.wav", NUM_CHANNELS*SAMPLE_RATE*RECORD_DURATION, NUM_CHANNELS, buffer, SAMPLE_RATE, BPS);
+
+    // for (int i = 0; i < TRANSFER_RUNS; i++) {
+        // printf("Frame %d:\n", i);
+        // parsemem(frames[i], TRANSFER_LEN);
+        // printf("==============================\n");
+    // }
     
     audio_i2s_release(&my_config);
-
+    fclose(fptr);
     return 0;
 }
